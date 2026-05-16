dde6feef949f16a6d4ae59e087a31e148b850214
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2026-05-16 13:54:44 +0700

    Image Deployment Issues #2

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,19 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const isProd = process.env.NODE_ENV === 'production';
 
-let repo = "";
-if (isGithubActions) {
-  // Extract your repository name from the GITHUB_REPOSITORY environment variable
-  // e.g., "username/portfolio" -> "portfolio"
-  repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "") || "";
-}
+// Update this to match your GitHub repository name
+const repoName = "portfolio-2026";
 
 const nextConfig: NextConfig = {
-  // config options here 
+  /* config options here */
   reactCompiler: true,
-  
-  // 1. Enable Static Export
   output: "export",
-
-  // 2. Disable Image Optimization for next/image
   images: {
     unoptimized: true,
   },
-
-  // 3. Set basePath if deployed to username.github.io/repo-name
-  // (Remove this if you are using a custom domain)
-  basePath: repo ? `/${repo}` : "",
-  assetPrefix: repo ? `/${repo}/` : "",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
